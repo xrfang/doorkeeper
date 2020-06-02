2270ab53595868ff61a7fe48bae9610a892bc012
xrfang <xrfang@gmail.com>
2020-06-02 09:42:27 +0800

    added command line usage info

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	ver := flag.Bool("version", false, "Show version info")
 	cfg := flag.String("conf", "", "Configuration file")
+	flag.Usage = func() {
+		fmt.Printf("DoorKeeper %s\n\n", verinfo())
+		fmt.Printf("USAGE: %s [OPTIONS]\n\n", filepath.Base(os.Args[0]))
+		fmt.Println("OPTIONS:\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 	if *ver {
 		fmt.Println(verinfo())
