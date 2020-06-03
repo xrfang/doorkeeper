e5fe14bd049655d055e59dbcdf956ba32fc0d15c
xrfang <xrfang@gmail.com>
2020-06-03 17:54:40 +0800

    WIP: validate connection

--- svr/link.go
+++ svr/link.go
@@ -19,11 +19,13 @@ func (l *link) setup(conn net.Conn, stat int) {
 }
 
 type peer struct {
-	role int //对端状态：0=未知；1=DK客户端（后端服务）；2=远端访问者
-	lnks []link
+	role  int //对端状态：0=未知；1=DK客户端（后端服务）；2=远端访问者
+	links []link
 }
 
 func (p *peer) validate() bool {
+	conn := p.links[len(p.links)-1]
+	conn.SetReadDeadline()
 	return false //TODO
 }
 
@@ -42,7 +44,7 @@ func (cm *connMgr) Register(conn net.Conn) {
 	host, _, _ := net.SplitHostPort(conn.RemoteAddr().String())
 	p := cm.peers[host]
 	if p == nil {
-		p = &peer{role: 0, lnks: []link{{stat: 0, conn: conn}}}
+		p = &peer{role: 0, links: []link{{stat: 0, conn: conn}}}
 		if p.validate() {
 			cm.peers[host] = p
 		}
@@ -52,12 +54,12 @@ func (cm *connMgr) Register(conn net.Conn) {
 	case 0: //一个等待确认身份的远端又一次发起连接
 		conn.Close() //关闭该连接，不予理会
 	case 1: //一个后端服务再次发起新的主控连接
-		p.lnks = append(p.lnks, link{stat: 0, conn: conn})
+		p.links = append(p.links, link{stat: 0, conn: conn})
 		if p.validate() { //新建主控连接仍然需要握手认证
 			cm.peers[host] = p
 		}
 	case 2: //一个远端客户发起连接
-		p.lnks = append(p.lnks, link{stat: 1, conn: conn})
+		p.links = append(p.links, link{stat: 1, conn: conn})
 		p.relay() //为该连接启动转发服务
 		cm.peers[host] = p
 	}
