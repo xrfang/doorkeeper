7dc14bb01e7b730ad543811b44d73bb883d58bfe
xrfang <xrfang@gmail.com>
2020-06-21 13:48:52 +0800

    api: implemented /help

--- svr/admin.go
+++ svr/admin.go
@@ -8,10 +8,17 @@ import (
 	"time"
 )
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not found", http.StatusNotFound)
+}
+
 func startAdminIntf(cf Config) {
 	http.HandleFunc("/", controller(cf))
+	http.HandleFunc("/help", notFound)
 	http.HandleFunc("/help/", helper(cf))
+	http.HandleFunc("/auths", notFound)
 	http.HandleFunc("/auths/", auths(cf))
+	http.HandleFunc("/conns", notFound)
 	http.HandleFunc("/conns/", conns(cf))
 	adm := http.Server{
 		Addr:         fmt.Sprintf(":%d", cf.AdminPort),
--- svr/help.go
+++ svr/help.go
@@ -1,20 +1,54 @@
 package svr
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/pquerna/otp/totp"
 )
 
+const helptxt = `
+How to use DoorKeepr
+
+1. Request access to a site:
+   - request SSH access for the gateway host of site "test":
+     curl dksvr:<PADM>/<otp>/test/22
+   - request RDP access for host "192.168.1.9" in site "test":
+     curl dksvr:<PADM>/<otp>/test/3389/192.168.1.9
+2. Connect to the site
+   - SSH to the gateway just requested access:
+     ssh -p<PWRK> dksvr
+   - RDP to the host just requested access:
+     rdesktop dksvr:<PWRK> 
+3. Manage sites
+   - list sites: 
+     curl dksvr:<PADM>/<otp>
+   - list hosts in site "test" with SSH port open:
+     curl dksvr:<PADM>/<otp>/test/22/*
+4. Manage connections
+   - list active connections to site "test":
+     curl dksvr:<PADM>/conns/<otp>/test
+   - terminate all connections to site "test":
+     curl dksvr:<PADM>/conns/<otp>/test?delete
+5. Manage access tokens
+   - list active access grants:
+     curl dksvr:<PADM>/auths/<otp>
+   - revoke all access grants:
+     curl dksvr:<PADM>/auths/<otp>?delete
+`
+
 func helper(cf Config) func(http.ResponseWriter, *http.Request) {
+	usage := strings.ReplaceAll(helptxt, "<PADM>", strconv.Itoa(cf.AdminPort))
+	usage = strings.ReplaceAll(usage, "<PWRK>", strconv.Itoa(cf.ServePort))
 	return func(w http.ResponseWriter, r *http.Request) {
-		path := strings.Trim(r.URL.Path, " /\t")
-		s := strings.Split(path, "/")
-		if len(s) == 0 || !totp.Validate(s[0], cf.OTP.Key) {
+		path := strings.Trim(r.URL.Path[6:], " /\t")
+		if !totp.Validate(path, cf.OTP.Key) {
 			http.Error(w, "not found", http.StatusNotFound)
 			return
 		}
-		http.Error(w, "Not Implemented", http.StatusNotImplemented)
+		w.Header().Set("Content-Type", "text/plain; charset=en_US.UTF-8")
+		fmt.Fprintln(w, usage)
 	}
 }
